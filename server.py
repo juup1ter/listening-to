cc1f9038a669ab6f69f56a5d0ef51b122b7277b8
jupiter <88718409+juup1ter@users.noreply.github.com>
2021-08-12 21:12:17 +1000

    colour coded console messages

--- server.py
+++ server.py
@@ -5,12 +5,22 @@ import pickle
 import requests
 import pypresence
 import discord
+import os
+#import colorama
+#from colorama import Fore, Style, Back, init
 from discord.ext import commands
 from pypresence import presence
 
 LASTFM_API_URL = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={key}&format=json"
 DISCORD_API_POST_URL = "https://discord.com/api/v8/oauth2/applications/{client_id}/assets"
 
+os.system("")
+class colour():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    RESET = '\033[0m'
 
 def main():
     try:
@@ -29,9 +39,9 @@ def main():
     lfmimg = config['lfmimg']
         
     if lfm in album_cache and lfm:
-        print("small image found! skipping first cache process...\n")
+        print(colour.GREEN + "small image found! skipping first cache process...\n" + colour.RESET)
     else:
-        print("small image not found! caching...")
+        print(colour.YELLOW + "attn: small image not found! caching..." + colour.RESET)
         requests.post(DISCORD_API_POST_URL.format(client_id=config["client_id"]),
                       json={"name": "lfm",
                             "image": lfmimg,
@@ -41,7 +51,7 @@ def main():
         album_cache.append("lfm")
         with open("album_cache.p", "wb") as f:
             pickle.dump(album_cache, f)
-        print("cached successfully!\n")
+        print(colour.GREEN + "cached successfully!\n" + colour.RESET)
 
     rpc = pypresence.Presence(config["client_id"], pipe=0)
     rpc.connect()
@@ -62,7 +72,7 @@ def main():
             album_discord = ''.join([str(ord(x) - 96) for x in album_name])[:32]
             
             if album_discord not in album_cache and album_discord:
-                print(f"{album_text} not found in album cache, caching...")
+                print(colour.YELLOW + f"attn: {album_text} not found in album cache, caching..." + colour.RESET)
                 cover_img = requests.get(
                     trackinfo["image"][1]["#text"]).content
                 cover_img = "data:image/jpeg;base64," + \
@@ -73,12 +83,12 @@ def main():
                                     "type": 1},
                               headers={"Authorization": config["discord_token"],
                                        "content-type": "application/json"})
-                print(f"converted {album_text} to {album_discord} successfully!")
-                print(f"{album_text} sent to discord correctly!")
+                print(colour.GREEN + f"converted {album_text} to {album_discord} successfully!" + colour.RESET)
+                print(colour.GREEN + f"{album_text} sent to discord correctly!" + colour.RESET)
                 album_cache.append(album_discord)
                 with open("album_cache.p", "wb") as f:
                     pickle.dump(album_cache, f)
-                print("cached succesfully!\n")
+                print(colour.GREEN + "cached succesfully!\n" + colour.RESET)
 
             rpc.update(details=f"{track_display}",
                        state=f"by {trackinfo['artist']['#text']}",
@@ -88,15 +98,16 @@ def main():
                        large_text=album_display if album_display else None)
             
             if old_trackname != trackinfo["name"]:
-                print(f"updating rpc with current track {trackinfo['name']}...")
+                print(colour.YELLOW + f"attn: updating rpc with current track {trackinfo['name']}..." + colour.RESET)
                 old_trackname = trackinfo["name"]
-                print("successfully set rpc!\n")
+                print(colour.GREEN + "successfully set rpc!\n" + colour.RESET)
+                print("waiting for next track...\n")
 
             time.sleep(0.3)
 
         except Exception as e:
-            print("exception occurred:", e)
-            print("skipping generic lastfm error: 'recenttracks', you can ignore this\n")
+            print(colour.RED + "exception occurred:", e + colour.RESET)
+            print(colour.YELLOW + "attn: skipping generic lastfm error: 'recenttracks', you can ignore this\n" + colour.RESET)
 
 if __name__ == '__main__':
     print("welcome to listening-to!\n")
